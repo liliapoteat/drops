e604bc9c7376d1cb1d99040f1fbd17bd611e5414
Claire Nord <cnord@mit.edu>
2017-06-24 21:54:18 -0400

    Make menu!
    Based on https://github.com/MattMcFarland/phaser-menu-system/

--- main.js
+++ main.js
@@ -1,67 +1,22 @@
-window.onload = function() {
-    //  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
-    //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
-    //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
+var
+  game = new Phaser.Game(1080, 1920, Phaser.AUTO, 'game'),
+  Main = function () {};
 
-    var game = new Phaser.Game(1080, 1920, Phaser.AUTO, '', { preload: preload, create: create });
+Main.prototype = {
+  preload: function () {
+    game.load.image('bg', 'assets/img/backgrounds_boston.png');
+    game.load.image('loading', 'assets/img/loading.png');
+    game.load.image('logo', 'assets/img/drops_blue.png');
+    game.load.script('utils', 'lib/utils.js');
+    game.load.script('splash', 'states/Splash.js');
+  },
 
-    function preload () {
-
-        game.load.image('bl_drop', 'img/drops_blue.png');
-        game.load.image('br_drop', 'img/drops_brown.png');
-        game.load.image('gr_drop', 'img/drops_green.png');
-        game.load.image('bucket', 'img/clean_low.png');
-        var cursors;
-        var bucket;
-    }
-
-    function create () {
-        //create drops
-        percent_blue = 40;
-        percent_brown = 20;
-        percent_green = 100 - percent_blue - percent_brown;
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        drops = game.add.group();
-        drops.enableBody = true;
-        drops.physicsBodyType = Phaser.Physics.ARCADE;
-        drops.createMultiple(percent_blue/100, 'bl_drop');
-        drops.createMultiple(percent_brown/100, 'br_drop');
-        drops.createMultiple(percent_green/100, 'gr_drop');
-
-        //create bucket
-        cursors = game.input.keyboard.createCursorKeys();
-        bucket = game.add.sprite(game.world.centerX, 1750, 'bucket');
-        bucket.scale.setTo(0.02, 0.02);
-        game.physics.enable(bucket, Phaser.Physics.ARCADE);
-        // reset bucket velocity
-        bucket.body.velocity.x = 0;
-        //move this out of create for game
-
-        //test drops method
-        createDrops(400);
-
-    }
-
-    function createDrops(drop_speed){
-        drop = drops.getFirstExists(false);
-        if(drop){
-            drop.reset(0, 0);
-            drop.body.velocity.y = drop_speed;
-        }
-    }
-
-    function update () {
-
-        // user presses arrow keys --> control bucket movement
-        if (cursors.left.isDown) {
-            bucket.body.velocity.x = -200;
-        }
-        else if (cursors.right.isDown) {
-            bucket.body.velocity.x = 200;
-        }
-        else {
-            bucket.body.velocity.x = 0;
-        }
-    }
+  create: function() {
+    game.state.add('Splash', Splash);
+    game.state.start('Splash');
+  }
 
 };
+
+game.state.add('Main', Main);
+game.state.start('Main');
---
+++ states/Splash.js
@@ -0,0 +1,71 @@
+var Splash = function () {};
+
+Splash.prototype = {
+  loadScripts: function() {
+    game.load.script('WebFont', 'vendor/webfontloader.js');
+    game.load.script('menu', 'states/menu.js');
+    game.load.script('game', 'states/game.js');
+    game.load.script('levelreview', 'states/levelreview.js');
+    game.load.script('sources', 'states/sources.js');
+    game.load.script('leaderboard', 'states/leaderboard.js');
+  },
+
+  loadBgm: function() {
+    // background music
+    //game.load.audio('shortname', 'path/to/file.mp3');
+  },
+
+  loadImages: function() {
+    game.load.image('menu-bg', 'assets/img/backgrounds_boston.png');
+    //load other images, like options or game over
+  },
+
+  loadFonts: function() {
+    WebFontConfig = {
+      custom: {
+        families: ['Karla'],
+        urls: ['assets/style/karla.css']
+      }
+    }
+  },
+
+  init: function () {
+    this.loading = game.make.sprite(game.world.centerX, 400, 'loading');
+    this.logo = game.make.sprite(game.world.centerX, 200, 'logo');
+    this.status = game.make.text(game.world.centerX, 380, 'Loading...', {fill: 'white'});
+    utils.centerGameObjects([this.loading, this.logo, this.status]);
+  },
+
+  // The preload function will then call all of the previously defined fns
+  preload: function() {
+    game.add.sprite(0, 0, 'bg');
+    game.add.existing(this.logo);
+    game.add.existing(this.loading);
+    game.add.existing(this.status);
+
+    // Phaser will use loading as preload progress bar
+    this.load.setPreloadSprite(this.loading);
+    this.loadScripts();
+    this.loadImages();
+    this.loadFonts();
+    this.loadBgm();
+  },
+
+  addGameStates: function() {
+    game.state.add('menu', Menu);
+    game.state.add('game', Game);
+    game.state.add('levelreview', LevelReview);
+    game.state.add('sources', Sources);
+    game.state.add('leaderboard', Leaderboard);
+  },
+
+  create: function() {
+    this.status.setText('Ready!');
+    this.addGameStates();
+
+    setTimeout(function() {
+      console.log('starting game!!!!');
+      game.state.start('menu');
+    }, 1000);
+  }
+};

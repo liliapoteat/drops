bb7a9ec7623e2390d9987ee01651126d0fda4e66
Sarah Powazek <powazek@mit.edu>
2017-06-25 11:42:13 -0400

    Add preview screens

---
+++ states/preview1.js
@@ -0,0 +1,32 @@
+var Preview1 = function() {};
+
+Preview1.prototype = {
+
+  create: function() {
+    var background = game.add.sprite(0, 0, 'bg');
+    this.titleText = game.add.text(game.world.centerX, 480, 'level 1: Boston, MA', {
+      font: '60pt Karla',
+      fill: '#404040',
+      align: 'center'
+    });
+    this.titleText.anchor.set(0.5);
+
+    var text = "\n\nWelcome to Boston! \nCities like Boston have \naccess to reservoirs \nand high quality \ntreatment plants. \n\nIn the last 5 years, \n98% of water samples \nhad safe levels\nof lead."
+    var score_text = game.add.text(game.world.centerX, game.world.centerY, text, {
+      font: '60pt Karla-Bold',
+      fill: '#404040',
+      align: 'center'
+    });
+    score_text.anchor.set(0.5);
+  },
+  
+  update: function() {
+    if (game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR)) {
+      drops_collected=0;
+      blue_collected=0;
+      green_collected=0;
+      brown_collected=0;
+      game.state.start('game1');
+    }
+  }
+};
--- states/preview3.js
+++ states/preview3.js
@@ -1,19 +1,17 @@
-var Level1Review = function() {};
+var Preview3 = function() {};
 
-Level1Review.prototype = {
+Preview3.prototype = {
 
 	create: function() {
 		var background = game.add.sprite(0, 0, 'bg');
-		this.titleText = game.add.text(game.world.centerX, 480, 'level 1', {
+		this.titleText = game.add.text(game.world.centerX, 480, 'level 3: Sebring, OH', {
 			font: '60pt Karla',
 			fill: '#404040',
 			align: 'center'
 		});
 		this.titleText.anchor.set(0.5);
 
-    this.clean_percentage = (blue_collected + green_collected) / (drops_collected);
-
-    var text = "Your bucket is \n" + this.clean_percentage + "\n % clean";
+    var text = "\n\n\nWelcome to Sebring! \nIn the US, \n6-8 million homes \nstill get water \nfrom lead pipelines. \n\nThese corrode and \ninfect water toxins, \nleading to chronic \nhealth problems for \nfamilies and children.";
     var score_text = game.add.text(game.world.centerX, game.world.centerY, text, {
       font: '60pt Karla-Bold',
       fill: '#404040',
@@ -28,7 +26,7 @@ Level1Review.prototype = {
       blue_collected=0;
       green_collected=0;
       brown_collected=0;
-      game.state.start('preview2');
+      game.state.start('game3');
     }
   }
 };
